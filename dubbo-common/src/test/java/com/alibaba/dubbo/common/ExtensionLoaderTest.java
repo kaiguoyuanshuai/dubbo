601bfe090273d5fbbc0c1369740fa8e5cda1bd06
zcoco <zheng_keke@126.com>
2018-08-27 17:26:08 +0800

    初始化 dubbo的源码
    初始化 dubbo的源码

---
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/ExtensionLoaderTest.java
@@ -0,0 +1,11 @@
+package com.alibaba.dubbo.common;
+
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+
+/**
+ * Created by Administrator on 2018/6/21.
+ */
+public class ExtensionLoaderTest {
+    public static void main(String[] args) {
+    }
+}
